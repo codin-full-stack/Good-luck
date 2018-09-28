051a3b9231f070563e9cf8474097db06d4328b7d
Mindaugas <mindaugas.mazeikis1@gmail.com>
2018-09-28 23:03:34 +0300

    2018.09.28 comment delete/update

---
+++ userInfoDB.php
@@ -0,0 +1,36 @@
+<?php
+include 'header.php';
+?>
+
+<?php
+
+if (isset($_SESSION['id'])){
+    $sql = "select * from users where id='".$_SESSION['id']."' ";
+    
+    // var_dump($sql);
+
+    $result = $con->query($sql);
+    $user = $result->fetch_assoc(); 
+
+    // var_dump($user);
+
+    if(!empty($user)){
+        foreach ($user as $key => $value) {
+            echo $key . ': ' . $value . '<br>';
+        }    
+    }
+?>
+    
+<input type="button" value="Update Profile Info" onclick="location.href='updateUserInfo.php'" />
+<input type="button" value="Change password" onclick="location.href='passChange.php'" />
+
+<?php
+} else {
+    echo 'Norint perziureti informacija, butina prisijungti!';
+}
+?>
+
+
+<?php
+include 'footer.php';
+?>
